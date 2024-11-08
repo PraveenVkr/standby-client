aedcd01a681a7a7c8cbba5edf02f56ed86997505
Praveen <praveenvkr21@gmail.com>
2024-11-08 18:46:36 +0530

    Logo change and added the Footer

--- src/RegisterAndLoginForm.jsx
+++ src/RegisterAndLoginForm.jsx
@@ -44,50 +44,55 @@ export default function RegisterAndLoginForm() {
   }
 
   return (
-    <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
-        <input
-          value={username}
-          onChange={(ev) => setUsername(ev.target.value)}
-          type="text"
-          placeholder="username"
-          className="block w-full rounded-sm p-2 mb-2 border"
-        />
-        <input
-          value={password}
-          onChange={(ev) => setPassword(ev.target.value)}
-          type="password"
-          placeholder="password"
-          className="block w-full rounded-sm p-2 mb-2 border"
-        />
-        <button className="bg-blue-500 text-white block w-full rounded-sm p-2  hover:bg-blue-600 active:bg-blue-700 transition duration-200 ease-in-out">
-          {isLoginOrRegister === "register" ? "Register" : "Login"}
-        </button>
-        <div className="text-center mt-2">
-          {isLoginOrRegister === "register" && (
-            <div>
-              Already a member?
-              <button
-                className="ml-1"
-                onClick={() => setIsLoginOrRegister("login")}
-              >
-                Login here
-              </button>
-            </div>
-          )}
-          {isLoginOrRegister === "login" && (
-            <div>
-              Don't have an account?
-              <button
-                className="ml-1"
-                onClick={() => setIsLoginOrRegister("register")}
-              >
-                Register
-              </button>
-            </div>
-          )}
-        </div>
-      </form>
+    <div>
+      <div className="bg-blue-50 h-screen flex items-center">
+        <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
+          <input
+            value={username}
+            onChange={(ev) => setUsername(ev.target.value)}
+            type="text"
+            placeholder="username"
+            className="block w-full rounded-sm p-2 mb-2 border"
+          />
+          <input
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+            type="password"
+            placeholder="password"
+            className="block w-full rounded-sm p-2 mb-2 border"
+          />
+          <button className="bg-blue-500 text-white block w-full rounded-sm p-2  hover:bg-blue-600 active:bg-blue-700 transition duration-200 ease-in-out">
+            {isLoginOrRegister === "register" ? "Register" : "Login"}
+          </button>
+          <div className="text-center mt-2">
+            {isLoginOrRegister === "register" && (
+              <div>
+                Already a member?
+                <button
+                  className="ml-1"
+                  onClick={() => setIsLoginOrRegister("login")}
+                >
+                  Login here
+                </button>
+              </div>
+            )}
+            {isLoginOrRegister === "login" && (
+              <div>
+                Don't have an account?
+                <button
+                  className="ml-1"
+                  onClick={() => setIsLoginOrRegister("register")}
+                >
+                  Register
+                </button>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+      <footer className="fixed bottom-0 w-full flex justify-center bg-gray-700 text-white">
+        Made with 💙
+      </footer>
     </div>
   );
 }
